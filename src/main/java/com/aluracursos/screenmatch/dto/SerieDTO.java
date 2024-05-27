b64b5d098de80b8a8395a3482bb8232b190c0792
Luis Vega <l2vega@live.com>
2024-05-26 19:59:37 -0600

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	src/main/java/com/aluracursos/screenmatch/ScreenmatchApplicationConsola.java

--- src/main/java/com/aluracursos/screenmatch/dto/SerieDTO.java
+++ src/main/java/com/aluracursos/screenmatch/dto/SerieDTO.java
@@ -1,18 +1,15 @@
 package com.aluracursos.screenmatch.dto;
 
 import com.aluracursos.screenmatch.model.Categoria;
-import com.aluracursos.screenmatch.model.Episodio;
-import jakarta.persistence.*;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 
-import java.util.List;
-
-public record SerieDTO(
-        String titulo,
-        Integer totalTemporadas,
-        Double evaluacion,
-        String poster,
-        Categoria genero,
-        String actores,
-        String sinopsis){
-
+public record SerieDTO(Long id,
+         String titulo,
+         Integer totalTemporadas,
+         Double evaluacion,
+         String poster,
+         Categoria genero,
+         String actores,
+         String sinopsis){
 }
